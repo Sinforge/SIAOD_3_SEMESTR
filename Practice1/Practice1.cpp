c97bf08ec449bcef43637c5362fb44cc66fe02aa
Sinforge <vlad.vlasov77@mail.ru>
2022-09-01 21:32:23 +0300

    End first practice

--- Practice1/Practice1.cpp
+++ Practice1/Practice1.cpp
@@ -1,20 +1,130 @@
-﻿// Practice1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
+//VARIANT 4
 #include <iostream>
+void Ex_1_change_bit_to_1(unsigned int & number);
+void Ex_2_change_bit_to_0(unsigned int& number);
+void Ex_3_multiply_by_32(unsigned int& number);
+void Ex_4_divide_by_32(unsigned int& number);
+void Ex_5(unsigned int& number, short bite_number);
+
+void coutp(unsigned int x);
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//First
+	//DEFAULT = 0, A=10, B=11, C=12, D=13, E=14, F=15
+	//    0000 0000 0000
+	std::cout << "Ex.1\n";
+	unsigned int DEFAULT = 0x000;
+	Ex_1_change_bit_to_1(DEFAULT);
+
+	std::cout << std::endl;
+
+
+	//Second
+	//User input
+	/*std::cout << "Ex.2\n";
+	std::cout << "Input number for ex.2\n";
+	unsigned int NUMBER_FOR_2;
+	std::cin >> NUMBER_FOR_2;
+	Ex_2_change_bit_to_0(NUMBER_FOR_2);
+
+
+	std::cout << std::endl;
+
+	//Third
+	//User input
+	std::cout << "Ex.3\n";
+	unsigned int NUMBER_FOR_3;
+	std::cout << "Input number for ex.3\n";
+	std::cin >> NUMBER_FOR_3;
+	Ex_3_multiply_by_32(NUMBER_FOR_3);
+	std::cout << NUMBER_FOR_3;
+
+
+	std::cout << std::endl;
+
+	//Fouth
+	//User input
+	std::cout << "Ex.4\n";
+	unsigned int NUMBER_FOR_4;
+	std::cout << "Input number for ex.4\n";
+	std::cin >> NUMBER_FOR_4;
+	Ex_4_divide_by_32(NUMBER_FOR_4);
+	std::cout << NUMBER_FOR_4;
+	*/
+
+	//Fifth
+	//User input
+	std::cout << "Ex.5\n";
+	unsigned int NUMBER_FOR_5;
+	short bite_number;
+	std::cout << "Input number and bite position,which u want to change\n";
+	std::cin >> NUMBER_FOR_5 >> bite_number;
+	Ex_5(NUMBER_FOR_5, bite_number);
+	
+	
+
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+/*params
+	
+*/
+//Bit is 0
+void Ex_1_change_bit_to_1(unsigned int & number) {
+	// We need change uneven bits to 1
+	// Mask will be 1010 1010 1010
+	const unsigned int mask = 0xAAA;
+	number = number | mask;
+	coutp(number);
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+void Ex_2_change_bit_to_0(unsigned int& number) {
+	//We need reset 4 bits from 5 to the left
+	// 1111 .... 1111 1100 0011 1111
+	// F... F C 3 F
+	const unsigned int mask = 0xFFFFFFFFFFFFFC3F;
+	number = number & mask;
+	coutp(number);
+}
+
+
+void Ex_3_multiply_by_32(unsigned int& number) {
+	// number << 5 is equals number * 32
+	number = number << 5;
+}
+
+void Ex_4_divide_by_32(unsigned int& number) {
+	// number >> 5 is equals number / 32
+	number = number >> 5;
+}
+
+
+//Use if bite on position bite_number is not 1
+void Ex_5(unsigned int& number, short bite_number) {
+	//We have mask 1000000...0...00000
+	//Should get mask 0000000...1...00000. Use >> 
+	
+	int mask = INT_MAX - (INT_MAX >> 1);
+	short shift_to_right = sizeof(int) * 8 - bite_number - 2; //sizeof(int) = 4, 4 * 8 =32
+	number = number | (mask >> shift_to_right);
+	coutp(number);
+
+}
+
+void coutp(unsigned int x)
+{
+	int n = sizeof(int) * 8;
+	unsigned  maska = (1 << (n - 1));
+
+	for (int i = 1; i <= n; i++)
+	{
+		std::cout << ((x & maska) >> (n - i));
+		maska = maska >> 1;
+	}
+}
+
+

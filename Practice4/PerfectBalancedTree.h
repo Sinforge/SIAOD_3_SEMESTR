3cad85992dbf31579e77453f2d00b1274d8834b7
Sinforge <vlad.vlasov77@mail.ru>
2022-10-23 15:22:00 +0300

    fix bug with copy and add menu

---
+++ Practice4/PerfectBalancedTree.h
@@ -0,0 +1,164 @@
+#pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class TreeNode {
+private:
+    int info = 0;
+    int level = 0;
+    TreeNode* left, * right;
+public:
+    TreeNode() {}
+
+    TreeNode(int info, int level) {
+        this->info = info;
+        this->level = level;
+    }
+    void setLeft(TreeNode* left) {
+        this->left = left;
+    }
+    void setRight(TreeNode* right) {
+        this->right = right;
+    }
+    TreeNode* getLeft() {
+        return this->left;
+    }
+    TreeNode* getRight() {
+        return this->right;
+    }
+    int getLevel() {
+        return this->level;
+    }
+    int getInfo() {
+        return this->info;
+    }
+    void setInfo(int info) {
+        this->info = info;
+    }
+};
+
+class PerfectBalancedTree {
+private:
+    int size = 0;
+    vector<int> numbers;
+    TreeNode* topOfTree = new TreeNode(1, 0);
+
+    void insertNewNodeByRandom(int countNodes, TreeNode* currentNode) {
+        static int pos = 1;
+        if (countNodes == 1) {
+            int info;
+            info = rand() % 20;
+            this->numbers.push_back(info);
+
+            info = numbers.at(pos);
+            pos++;
+
+            currentNode->setLeft(new TreeNode(info, currentNode->getLevel() + 1));
+        }
+        else if (countNodes >= 2) {
+            int info1;
+            int info2;
+            info1 = rand() % 20;
+            info2 = rand() % 20;
+            numbers.push_back(info1);
+            numbers.push_back(info2);
+
+            info1 = numbers.at(pos);
+            info2 = numbers.at(pos + 1);
+            pos += 2;
+            currentNode->setLeft(new TreeNode(info1, currentNode->getLevel() + 1));
+            currentNode->setRight(new TreeNode(info2, currentNode->getLevel() + 1));
+
+        }
+        if (countNodes > 2) {
+            insertNewNodeByRandom((countNodes - 2) - ((countNodes - 2) / 2), currentNode->getLeft());
+            insertNewNodeByRandom(((countNodes - 2) / 2), currentNode->getRight());
+        }
+    }
+    void insertNewNodeByMassive(int countNodes, TreeNode* currentNode) {
+        static int pos = 1;
+        if (countNodes == 1) {
+            int info;
+            info = numbers.at(pos);
+            pos++;
+
+            currentNode->setLeft(new TreeNode(info, currentNode->getLevel() + 1));
+        }
+        else if (countNodes >= 2) {
+            int info1;
+            int info2;
+            info1 = numbers.at(pos);
+            info2 = numbers.at(pos + 1);
+            pos += 2;
+            currentNode->setLeft(new TreeNode(info1, currentNode->getLevel() + 1));
+            currentNode->setRight(new TreeNode(info2, currentNode->getLevel() + 1));
+
+        }
+        if (countNodes > 2) {
+            insertNewNodeByMassive((countNodes - 2) - ((countNodes - 2) / 2), currentNode->getLeft());
+            insertNewNodeByMassive(((countNodes - 2) / 2), currentNode->getRight());
+        }
+    }
+    void printNode(TreeNode* currentNode) {
+        if (currentNode)
+        {
+            printNode(currentNode->getRight());
+            for (int i = 0; i < currentNode->getLevel(); i++) cout << "-----";
+            cout << currentNode->getInfo() << endl;
+            printNode(currentNode->getLeft());
+        }
+
+    }
+    void getCountEvenNumbers(TreeNode* subtree, int& countEvenNumbers) {
+        if (!subtree) {
+            return;
+        }
+        if (subtree->getInfo() % 2 == 0) {
+            countEvenNumbers++;
+        }
+        getCountEvenNumbers(subtree->getRight(), countEvenNumbers);
+        getCountEvenNumbers(subtree->getLeft(), countEvenNumbers);
+    }
+public:
+    void MoreEvenNumbers() {
+        TreeNode* left = this->topOfTree->getLeft(),
+            * right = this->topOfTree->getRight();
+        int countLeft = 0, countRight = 0;
+        getCountEvenNumbers(left, countLeft);
+        getCountEvenNumbers(right, countRight);
+        if (countLeft == countRight) {
+            cout << "\nEquals amount\n";
+        }
+        else if (countLeft > countRight) {
+            cout << "\nLeft subtree have more even numbers\n";
+        }
+        else {
+            cout << "\nRight subtree have more even numbers\n";
+
+        }
+    }
+    PerfectBalancedTree* copyTree() {
+        return new PerfectBalancedTree(this->size, this->numbers);
+
+    }
+    PerfectBalancedTree(int nodeCount, vector<int> massive) {
+        this->numbers = massive;
+        this->size = nodeCount;
+        this->topOfTree->setInfo(massive[0]);
+        insertNewNodeByMassive(nodeCount - 1, this->topOfTree);
+
+    }
+    PerfectBalancedTree(int nodeCount) {
+        this->size = nodeCount;
+        int num = rand() % 20;
+        this->topOfTree->setInfo(num);
+        this->numbers.push_back(num);
+
+        insertNewNodeByRandom(nodeCount - 1, this->topOfTree);
+    }
+    void printTree() {
+        printNode(this->topOfTree);
+
+    }
+};
--- Practice4/Practice4.cpp
+++ Practice4/Practice4.cpp
@@ -1,176 +1,46 @@
 ﻿// Practice4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
-#include <iostream>
-#include <vector>
-using namespace std;
-
-class TreeNode {
-private:
-    int info = 0;
-    int level = 0;
-    TreeNode* left, * right;
-public:
-    TreeNode() {}
-
-    TreeNode(int info, int level) {
-        this->info = info;
-        this->level = level;
-    }
-    void setLeft(TreeNode* left) {
-        this->left = left;
-    }
-    void setRight(TreeNode* right) {
-        this->right = right;
-    }
-    TreeNode* getLeft() {
-        return this->left;
-    }
-    TreeNode* getRight() {
-        return this->right;
-    }
-    int getLevel() {
-        return this->level;
-    }
-    int getInfo() {
-        return this->info;
-    }
-    void setInfo(int info) {
-        this->info = info;
-    }
-};
-
-class PerfectBalancedTree {
-private:
-    int size = 0;
-    vector<int> numbers;
-    TreeNode* topOfTree = new TreeNode(1, 0);
-
-    void insertNewNode(int countNodes, TreeNode* currentNode) {
-        static int pos = 1;
-        if (countNodes == 1) {
-            int info;
-            if (numbers.size() != size-1) {
-                info = rand() % 20;
-                this->numbers.push_back(info);
-            }
-            else {
-                info = numbers.at(pos);
-                pos++;
-            }
-            currentNode->setLeft(new TreeNode(info, currentNode->getLevel() + 1));
-        }
-        else if (countNodes >= 2) {
-            int info1;
-            int info2;
-            if (numbers.size() != size - 1)
-            {
-                info1 = rand() % 20;
-                info2 = rand() % 20;
-                numbers.push_back(info1);
-                numbers.push_back(info2);
-            }
-            else {
-                info1 = numbers.at(pos);
-                info2 = numbers.at(pos + 1);
-                pos += 2;
-            }
-            currentNode->setLeft(new TreeNode(info1, currentNode->getLevel()+1));
-            currentNode->setRight(new TreeNode(info2, currentNode->getLevel() + 1));
-
-        }
-        if (countNodes > 2) {
-            insertNewNode((countNodes-2) - ((countNodes-2) / 2), currentNode->getLeft());
-            insertNewNode(((countNodes-2) / 2), currentNode->getRight());
-        }
-    }
-    void printNode(TreeNode* currentNode) {
-        if (currentNode)
-        {
-            printNode(currentNode->getRight());
-            for (int i = 0; i < currentNode->getLevel(); i++) cout << "-----";
-            cout << currentNode->getInfo() << endl;
-            printNode(currentNode->getLeft());
-        }
-
-    }
-    void getCountEvenNumbers(TreeNode* subtree, int & countEvenNumbers) {
-        if (!subtree) {
-            return;
-        }
-        if (subtree->getInfo() % 2 == 0) {
-            countEvenNumbers++;
-        }
-        getCountEvenNumbers(subtree->getRight(), countEvenNumbers);
-        getCountEvenNumbers(subtree->getLeft(), countEvenNumbers);
-    }
-
-    void getAllNumberFromTree(vector<int> & massive, TreeNode* currentNode) {
-        if (!currentNode) {
-            return;
-        }
-        massive.push_back(currentNode->getInfo());
-        getAllNumberFromTree(massive, currentNode->getRight());
-        getAllNumberFromTree(massive, currentNode->getLeft());
-    }
-public:
-    void MoreEvenNumbers() {
-        TreeNode* left = this->topOfTree->getLeft(),
-            * right = this->topOfTree->getRight();
-        int countLeft = 0, countRight = 0;
-        getCountEvenNumbers(left, countLeft);
-        getCountEvenNumbers(left, countRight);
-        if (countLeft == countRight) {
-            cout << "\nEquals amount\n";
-        }
-        else if (countLeft > countRight) {
-            cout << "\nLeft subtree have more even numbers\n";
-        }
-        else {
-            cout << "\nRight subtree have more even numbers\n";
-
-        }
-    }
-    PerfectBalancedTree* copyTree() {
-        return new PerfectBalancedTree(this->size, this->numbers);
-   
-    }
-    PerfectBalancedTree(int nodeCount, vector<int> massive) {
-        this->numbers = massive;
-        this->size = nodeCount;
-        this->topOfTree->setInfo(massive[0]);
-        for (int i = 0; i < numbers.size(); i++) {
-            cout << numbers.at(i) << " ";
-        }
-        cout << numbers.size() << " " << size;
-        insertNewNode(nodeCount - 1, this->topOfTree);
-
-    }
-    PerfectBalancedTree(int nodeCount) {
-        this->size = nodeCount;
-        int num = rand() % 20;
-        this->topOfTree->setInfo(num);
-        this->numbers.push_back(num);
-
-        insertNewNode(nodeCount-1, this->topOfTree);
-    }
-
-
-    
-
-    void printTree() {
-        printNode(this->topOfTree);
-
-    }
-};
-
+#include "PerfectBalancedTree.h"
 int main()
 {
-    PerfectBalancedTree * tree = new PerfectBalancedTree(10);
-    tree->printTree();
-    tree->MoreEvenNumbers();
-    PerfectBalancedTree* newTree = tree->copyTree();
-    newTree->printTree();
+
+   
+    setlocale(LC_ALL, "");
+    PerfectBalancedTree* tree = new PerfectBalancedTree(1);
+    PerfectBalancedTree* newTree = new PerfectBalancedTree(1);
+    int ex_num;
+    int size;
+    cout << "Практическая работа номер 4 \"Нелинейные структуры данных. Бинарное дерево.\"\nПреподаватель: Филатов Александр Сергеевич\nРаботу выполнил: Власов Владислав Витальевич ИКБО-10-21\n\n";
+    cout << "Введите номер задания, которое хотите протестировать: \n0)Выход из программы\n1)Создание идеального сбалансированного дерева\n2)Вывод дерева\n3)Определить в каком поддереве исходного дерева больше четных чисел\n4)Скопировать дерево\n5)Вывести скопированное дерево\n";
+    cin >> ex_num;
+    while (ex_num) {
+        switch (ex_num) {
+        case 1:
+            cout << "Input size of tree\n";
+            cin >> size;
+            tree = new PerfectBalancedTree(size);
+            break;
+        case 2:
+            tree->printTree();
+            break;
+        case 3:
+            tree->MoreEvenNumbers();
+            break;
+        case 4:
+            newTree = tree->copyTree();
+            break;
+        case 5:
+            newTree->printTree();
+            break;
+        default:
+            return 0;
+            break;
+        }
+        cout << "Введите номер задания, которое хотите протестировать: \n0)Выход из программы\n1)Создание идеального сбалансированного дерева\n2)Вывод дерева\n3)Определить в каком поддереве исходного дерева больше четных чисел\n4)Скопировать дерево\n5)Вывести скопированное дерево\n";
+        cin >> ex_num;
+
+    }
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"

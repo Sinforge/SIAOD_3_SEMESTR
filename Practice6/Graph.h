074d61b716cb4b98ceada274fdc2b2225248415a
Sinforge <vlad.vlasov77@mail.ru>
2022-11-15 01:10:22 +0300

    6th practice ended

--- Practice6/Graph.h
+++ Practice6/Graph.h
@@ -8,10 +8,15 @@ class Graph {
 	class Node {
 	public:
 		int id;
-		vector<Node*> nodes = vector<Node*>();
-		vector<int> lengthConnections = vector<int>();
+
+		//Pair of NodeId and Length 
+		vector<pair<int, int>> connections = vector<pair<int, int>>();
 		Node(int id) {
 			this->id = id;
+		}
+		Node(int id, pair<int, int> connection) {
+			this->id = id;
+			connections.push_back(connection);
 		}
 	};
 	vector<Node*> allNodes = vector<Node*>();
@@ -26,42 +31,32 @@ public:
 		
 	}*/
 
-	void AddNewNode(int connection, int length) {
-		Node * newNode = new Node(nodeCounter);
-		nodeCounter++;
+	void AddNewNode(pair<int, int> connection) {
+		//Create new node and and connection
+		Node * newNode = new Node(nodeCounter, connection);
+		//Add node to all nodes of Graph
 		allNodes.push_back(newNode);
-		//Add newNode to other nodes 
-		allNodes[connection]->nodes.push_back(newNode);
-		//Add length between nodes to other nodes
-		allNodes[connection]->lengthConnections.push_back(length);
+		//Add connection to other node
+		allNodes[connection.first]->connections.push_back(make_pair(nodeCounter, connection.second));
+		nodeCounter++;
 
-		//Add length between nodes to newNode
-		newNode->lengthConnections.push_back(length);
-
-		//Add other nodes to newNode
-		newNode->nodes.push_back(allNodes[connection]);
 
 	}
-	void AddNewNode(vector<int> connections, vector<int> lengths) {
+	void AddNewNode(vector<pair<int, int>> connections) {
 		Node * newNode = new Node(nodeCounter);
 		allNodes.push_back(newNode);
-		nodeCounter++;
 		for (int i = 0; i < connections.size(); i++) {
-
-			//Add newNode to other nodes 
-			allNodes[connections[i]]->nodes.push_back(newNode);
-			//Add length between nodes to other nodes
-			allNodes[connections[i]]->lengthConnections.push_back(lengths[i]);
-
-			//Add length between nodes to newNode
-			newNode->lengthConnections.push_back(lengths[i]);
-
-			//Add other nodes to newNode
-			newNode->nodes.push_back(allNodes[connections[i]]);
+			newNode->connections.push_back(connections[i]);
+			allNodes[connections[i].first]->connections.push_back(make_pair(nodeCounter, connections[i].second));
 		}
+		nodeCounter++;
+
 
 	}
-	void FindDiameter() {
+	void AddConnection(int id, pair<int, int> connection) {
+		allNodes[id]->connections.push_back(connection);
+	}
+	int FindDiameter() {
 		int inf = 420424;
 		vector<vector<int>> CostTable = vector<vector<int>>(allNodes.size());
 
@@ -82,29 +77,23 @@ public:
 
 		//Fill length
 		for (int i = 0; i < allNodes.size(); i++) {
-			for (int j = i; j < allNodes[i]->nodes.size(); j++) {
-				CostTable[i][allNodes[i]->nodes[j]->id] = allNodes[i]->lengthConnections[j];
-				CostTable[allNodes[i]->nodes[j]->id][i] = allNodes[i]->lengthConnections[j];
+			for (int j = i; j < allNodes[i]->connections.size(); j++) {
+				CostTable[i][allNodes[i]->connections[j].first] = allNodes[i]->connections[j].second;
+				CostTable[allNodes[i]->connections[j].first][i] = allNodes[i]->connections[j].second;
 			}
 		}
 		
 		//Get Floyd Table
 		FloydWarshallAlgorithm(CostTable);
-		for (int i = 0; i < CostTable.size(); i++) {
-			for (int j = 0; j < CostTable.size(); j++) {
-				cout << CostTable[i][j] << ' ';
-
-			}
-			cout << endl;
-		}
-
+	
+		//Get diameter
 		int diameter = -1;
 		for (int i = 0; i < CostTable.size(); i++) {
 			for (int j = i; j < CostTable.size(); j++) {
 				diameter = max(diameter, CostTable[i][j]);
 			}
 		}
-		cout << "Diameter equals: " << diameter;
+		return diameter;
 	}
 	void FloydWarshallAlgorithm(vector<vector<int>> & CostTable) {
 		for (int i = 0; i < CostTable.size(); i++) {
@@ -114,6 +103,92 @@ public:
 				}
 			}
 		}
+	}
+	void PrintGraph() {
+		cout << "\nYour Graph\n";
+		for (int i = 0; i < allNodes.size(); i++) {
+			cout << i << ":\t";
+			for (int j = 0; j < allNodes[i]->connections.size(); j++) {
+				cout << "(" << allNodes[i]->connections[j].first << ", " << allNodes[i]->connections[j].second << " ) ";
+			}
+			cout << endl;
+		}
+	}
+	 Graph PrimAlgorithm() {
+		//Choose Random node
+		int randomNumber = rand() % allNodes.size();
+
+		//Contains visited nodes id
+		vector<int> visited = vector<int>{randomNumber};
+		//Contains unvisited nodes
+		vector<int> unvisited = vector<int>();
+		
+		//Edge-length vector
+		vector<pair<pair<int, int>, int>> choosedEdges = vector<pair<pair<int, int>, int>>();
+
+		//Fill unvisited nodes
+		for (int i = 0; i < allNodes.size(); i++) {
+			if (i != randomNumber) {
+				unvisited.push_back(i);
+			}
+		}
+		
+
+		while (unvisited.size() != 0) {
+			int minLength = INT_MAX;
+			int minNodeId = -1;
+			//Edge - example (0, 1) or (2, 3), and length of edge
+			pair<pair<int, int>, int> foundedEdge = pair<pair<int, int>, int>();
+			for (int i = 0; i < visited.size(); i++) {
+				for (int j = 0; j < allNodes[visited[i]]->connections.size(); j++) {
+					if (!isVisited(visited, allNodes[visited[i]]->connections[j].first)) {
+						if (allNodes[visited[i]]->connections[j].second < minLength) {
+							minLength = allNodes[visited[i]]->connections[j].second;
+							minNodeId = allNodes[visited[i]]->connections[j].first;
+							foundedEdge.first = make_pair(visited[i], allNodes[visited[i]]->connections[j].first);
+						}
+					}
+				}
+			}
+			
+			foundedEdge.second = minLength;
+			choosedEdges.push_back(foundedEdge);
+			//Add to visited
+			visited.push_back(minNodeId);
+
+			//Delete from unvisited
+			for (int j = 0; j < unvisited.size(); j++) {
+				if (unvisited[j] == minNodeId) {
+					unvisited.erase(unvisited.begin() + j);
+					break;
+				}
+			}
+		}
+		Graph OstovTreeGraph = Graph();
+		vector<Node*> newNodes = vector<Node*>();
+		for (int i = 0; i < allNodes.size(); i++) {
+			newNodes.push_back(new Node(i));
+		}
+		OstovTreeGraph.allNodes = newNodes;
+	
+		for (int i = 0; i < choosedEdges.size(); i++) {
+			OstovTreeGraph.AddConnection(choosedEdges[i].first.first, make_pair(choosedEdges[i].first.second, choosedEdges[i].second));
+			OstovTreeGraph.AddConnection(choosedEdges[i].first.second, make_pair(choosedEdges[i].first.first, choosedEdges[i].second));
+		}
+
+		return OstovTreeGraph;
+		
+		
+	}
+	bool isVisited(vector<int> visited, int id) {
+		bool flag = false;
+		for (int i = 0; i < visited.size(); i++) {
+			if (visited[i] == id) {
+				flag = true;
+				break;
+			}
+		}
+		return flag;
 	}
 	
 
--- Practice6/Practice6.cpp
+++ Practice6/Practice6.cpp
@@ -5,13 +5,70 @@
 #include "Graph.h"
 int main()
 {
-	Graph graph = Graph();
-	graph.AddNewNode(0, 1);
-	graph.AddNewNode(0, 2);
-	graph.AddNewNode(vector<int>{1, 2}, vector<int>{3, 4});
-	graph.AddNewNode(vector<int>{0, 1, 2, 3}, vector<int>{10, 6, 7, 11});
+	setlocale(LC_ALL, "");
+	Graph graph;
+	int ex_num;
+	int connectionId;
+	int connectionId2;
+	int countConnections;
+	int connectionLength;
+	vector<pair<int, int>> connectionsList = vector<pair<int, int>>();
 
-	graph.FindDiameter();
+	cout << "Практическая работа номер 6 \"Основные алгоритмы работы с графами.\"\nПреподаватель: Филатов Александр Сергеевич\nРаботу выполнил: Власов Владислав Витальевич ИКБО-10-21\n\n";
+	cout << "Введите номер задания, которое хотите протестировать: \n0)Выход из программы\n1)Создать граф\n2)Добавить узел с одной связью\n3)Создать узел с несколькими связями\n4)Добавить новую связь между узлами\n5)Найти диаметр графа\n6)Получить остовное дерево\n7)Вывести граф\n";
+	cin >> ex_num;
+	while (ex_num) {
+		switch (ex_num) {
+		case 1:
+			graph = Graph();
+			cout << "\nGraph is created\n";
+			break;
+		case 2:
+			cout << "Enter connection Id and connection Length\n";
+			cin >> connectionId;
+			cin >> connectionLength;
+			graph.AddNewNode(make_pair(connectionId, connectionLength));
+			cout << "Node is added\n";
+			break;
+		case 3:
+			cout << "Enter how much connections node will be have\n";
+			connectionsList = vector<pair<int, int>>();
+
+			cin >> countConnections;
+			for (int i = 0; i < countConnections; i++) {
+				cout << "Enter connection Id and connection Length\n";
+				cin >> connectionId;
+				cin >> connectionLength;
+				connectionsList.push_back(make_pair(connectionId, connectionLength));
+			}
+			graph.AddNewNode(connectionsList);
+			cout << "Node is added\n";
+			break;
+		case 4:
+			cout << "Enter two nodes id and length\n";
+			cin >> connectionId >> connectionId2 >> connectionLength;
+			graph.AddConnection(connectionId, make_pair(connectionId2, connectionLength));
+			cout << "Connections is added\n";
+			break;
+		case 5:
+			cout << "Diameter equals: " << graph.FindDiameter() << endl;
+			break;
+		case 6:
+			graph.PrimAlgorithm().PrintGraph();
+			break;
+		case 7:
+			graph.PrintGraph();
+			break;
+
+
+
+		default:
+			return 0;
+			break;
+		}
+		cout << "Введите номер задания, которое хотите протестировать: \n0)Выход из программы\n1)Создать граф\n2)Добавить узел с одной связью\n3)Создать узел с несколькими связями\n4)Добавить новую связь между узлами\n5)Найти диаметр графа\n6)Получить остовное дерево\n7)Вывести граф\n";
+		cin >> ex_num;
+	}
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
